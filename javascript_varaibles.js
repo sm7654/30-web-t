9d51e7161dfa9c0b2c136c6c1c5d1a531198a3bd
sm7654 <115954704+sm7654@users.noreply.github.com>
2023-05-29 00:52:52 +0300

    Add files via upload

---
+++ javascript_varaibles.js
@@ -0,0 +1,16 @@
+let i = 0;
+let a = 0;
+const banner = document.getElementsByClassName("banner");
+const banner_height = document.getElementsByClassName("banner")[0].clientHeight - 80;
+const addToCart = document.getElementsByClassName("product_box_buttons_wraper");
+const login_cart = document.getElementsByClassName("li_header");
+const bars_wraper = document.getElementById("bars");
+const header = document.getElementById("header");
+const ResponsivMmenu = document.getElementsByClassName("responsive_menu");
+const bars = document.getElementsByClassName("bar_2_3");
+const icon = document.getElementById("icons_header");
+const height = window.scrollTop;
+const Contact_us_block_height = document.getElementsByClassName("Contact_us_block")[0].clientHeight;
+var height_contact_block = 0 - Contact_us_block_height / 2;
+const dropdown_options_wraper = document.getElementsByClassName("drop_down_menu")[0];
+const button_dropdwon = document.getElementsByClassName("button_dropdown")[0];

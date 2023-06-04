a1d93dcdd554efa6d0cc68f6094a9814919a5e26
sm7654 <115954704+sm7654@users.noreply.github.com>
2023-06-04 11:58:52 +0300

    Add files via upload

--- javascript_varaibles.js
+++ javascript_varaibles.js
@@ -1,5 +1,6 @@
 let i = 0;
 let a = 0;
+let Check = false;
 const banner = document.getElementsByClassName("banner");
 const banner_height = document.getElementsByClassName("banner")[0].clientHeight - 80;
 const addToCart = document.getElementsByClassName("product_box_buttons_wraper");
@@ -14,3 +15,38 @@ const Contact_us_block_height = document.getElementsByClassName("Contact_us_bloc
 var height_contact_block = 0 - Contact_us_block_height / 2;
 const dropdown_options_wraper = document.getElementsByClassName("drop_down_menu")[0];
 const button_dropdwon = document.getElementsByClassName("button_dropdown")[0];
+const text = document.getElementById("form_main_text");
+const form = document.getElementsByClassName("form_contact_us")[0];
+const button_send = document.getElementsByClassName("send_button_contact_us")[0];
+const email_value = document.getElementById("email");
+const name = document.getElementById("your_name");
+const invalidCharacters = [
+    " ",
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "*",
+    "+",
+    "=",
+    ",",
+    "/",
+    ":",
+    ";",
+    "<",
+    ">",
+    "?",
+    "[",
+    "]",
+    "\\",
+    "^",
+    "`",
+    "{",
+    "}",
+    "|",
+    "~",
+    '"',
+    "'"
+  ];
